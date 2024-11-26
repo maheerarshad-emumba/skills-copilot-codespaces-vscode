43fcfe0360e3b00c430756a37fc8b4f08c37eecd
maheerarshad-emumba <maheer.arshad@emumba.com>
2024-11-26 10:19:19 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,9 @@
+// Create web server
+// Create a web server that listens on port 3000 and serves the comments.json file using the fs module. If the file is not found, the server should respond with a 404 status code.
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(
