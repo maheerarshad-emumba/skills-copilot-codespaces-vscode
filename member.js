4a4a74cc522ea52c93e217f8884bbbcf861cbcb4
maheerarshad-emumba <maheer.arshad@emumba.com>
2024-11-26 10:11:24 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {}
+skillsMember.prototype = {
+  // This is a function to create a new member
+  createMember: function() {
+    console.log('Creating a new member');
+  }
+};
